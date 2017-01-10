716873feb6d2d22228b8bffeda9b3836235d1b60
hanyen <me@hanyen.com>
2017-01-09 16:30:41 -0800

    Able to get and console.log server response

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,6 +11,15 @@ this file and include it in basic-server.js so that it actually works.
 *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
 
 **************************************************************/
+//
+// Another way to get around this restriction is to serve you chat
+// client from this domain by setting up static file serving.
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10 // Seconds.
+};
 
 var requestHandler = function(request, response) {
   // Request and Response come from node's http module.
@@ -28,11 +37,12 @@ var requestHandler = function(request, response) {
   // debugging help, but you should always be careful about leaving stray
   // console.logs in your code.
   var data = require('./data');
+  
   var url = require('url');
   var fs = require('fs');
   // console.log('Serving request type ' + request.method + ' for url ' + request.url);
   var path = url.parse(request.url).pathname;
-  console.log("path", path);
+  console.log('path', path);
 
   // The outgoing status.
   var statusCode = 200;
@@ -67,15 +77,6 @@ var requestHandler = function(request, response) {
 //
 // Your chat client is running from a url like file://your/chat/client/index.html,
 // which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
-var defaultCorsHeaders = {
-  'access-control-allow-origin': '*',
-  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10 // Seconds.
-};
 
 module.exports.requestHandler = requestHandler;
 
